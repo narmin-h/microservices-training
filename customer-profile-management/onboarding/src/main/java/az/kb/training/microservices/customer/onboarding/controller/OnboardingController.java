afd23f03b55824b699418e924472cb125cc73b4a
HuseynliN <narmin.huseynli@kapitalbank.az>
2025-01-05 04:50:32 +0400

    Add producer -  spring kafka version

---
+++ customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/controller/OnboardingController.java
@@ -0,0 +1,29 @@
+package az.kb.training.microservices.customer.onboarding.controller;
+
+import az.kb.training.microservices.customer.onboarding.model.command.CreateCustomerCommand;
+import az.kb.training.microservices.customer.onboarding.service.CustomerOnboardingService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/onboarding/customers")
+public class OnboardingController {
+    private final CustomerOnboardingService customerOnboardingService;
+
+    @PostMapping("/customer")
+    public ResponseEntity<Void> createCustomer(@RequestBody CreateCustomerCommand createCustomerCommand) {
+        customerOnboardingService.createCustomer(createCustomerCommand);
+        return ResponseEntity.accepted().build();
+    }
+
+    @GetMapping("/customer")
+    public ResponseEntity<Void> test() {
+        return ResponseEntity.accepted().build();
+    }
+}
---
+++ customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/model/event/CustomerCreatedEvent.java
@@ -0,0 +1,14 @@
+package az.kb.training.microservices.customer.onboarding.model.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CustomerCreatedEvent {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
