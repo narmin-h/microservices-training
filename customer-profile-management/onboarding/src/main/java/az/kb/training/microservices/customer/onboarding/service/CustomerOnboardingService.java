3c18bd9757157d921576e5744c2b5e9d3e479999
HuseynliN <narmin.huseynli@kapitalbank.az>
2025-01-15 12:48:14 +0400

    Add outbox pattern

--- customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/service/CustomerOnboardingService.java
+++ customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/service/CustomerOnboardingService.java
@@ -3,8 +3,9 @@ package az.kb.training.microservices.customer.onboarding.service;
 
 import az.kb.training.microservices.customer.onboarding.enums.OnboardingStatus;
 import az.kb.training.microservices.customer.onboarding.model.command.CreateCustomerCommand;
+import com.fasterxml.jackson.core.JsonProcessingException;
 
 public interface CustomerOnboardingService {
-     void createCustomer(CreateCustomerCommand command);
+     void createCustomer(CreateCustomerCommand command) throws JsonProcessingException;
      void updateCustomer(Long id, OnboardingStatus status);
 }
--- customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/service/impl/KafkaCustomerService.java
+++ customer-profile-management/onboarding/src/main/java/az/kb/training/microservices/customer/onboarding/service/impl/KafkaCustomerService.java
@@ -2,6 +2,7 @@ package az.kb.training.microservices.customer.onboarding.service.impl;
 
 
 import static az.kb.training.microservices.customer.customercore.constant.KafkaConstants.CUSTOMER_CREATION_TOPIC;
+import static az.kb.training.microservices.customer.customercore.constant.KafkaConstants.CUSTOMER_VALIDATION_TOPIC;
 
 import az.kb.training.microservices.customer.customercore.component.KafkaPublisher;
 import az.kb.training.microservices.customer.customercore.constant.KafkaConstants;
@@ -10,9 +11,15 @@ import az.kb.training.microservices.customer.customercore.model.event.CustomerCr
 import az.kb.training.microservices.customer.onboarding.enums.OnboardingStatus;
 import az.kb.training.microservices.customer.onboarding.model.command.CreateCustomerCommand;
 import az.kb.training.microservices.customer.onboarding.model.entity.CustomerOnboarding;
+import az.kb.training.microservices.customer.onboarding.model.entity.Outbox;
 import az.kb.training.microservices.customer.onboarding.repository.CustomerOnboardingRepository;
+import az.kb.training.microservices.customer.onboarding.repository.OutboxRepository;
 import az.kb.training.microservices.customer.onboarding.service.CustomerOnboardingService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.security.SecureRandom;
 import java.util.Objects;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -28,22 +35,31 @@ import org.springframework.transaction.annotation.Transactional;
 @Primary
 public class KafkaCustomerService implements CustomerOnboardingService {
     private final CustomerOnboardingRepository customerOnboardingRepository;
+    private final OutboxRepository outboxRepository;
+
     private final KafkaPublisher kafkaPublisher;
+    private final ObjectMapper objectMapper;
+
 
     @Transactional
     @Override
-    public void createCustomer(CreateCustomerCommand command) {
+    public void createCustomer(CreateCustomerCommand command) throws JsonProcessingException {
         var onboardingCustomer = new CustomerOnboarding();
         var fullName = command.getFirstName() + "" + command.getLastName();
         onboardingCustomer.setName(fullName);
         onboardingCustomer.setEmail(command.getEmail());
         onboardingCustomer.setStatus(OnboardingStatus.PENDING);
 
-        customerOnboardingRepository.save(onboardingCustomer);
+        onboardingCustomer = customerOnboardingRepository.save(onboardingCustomer);
 
+        var id = new SecureRandom().nextLong(1000L);
         var customerCreatedEvent =
-                new CustomerCreatedEvent(onboardingCustomer.getId(), fullName);
-        kafkaPublisher.publishMessage(CUSTOMER_CREATION_TOPIC, customerCreatedEvent);
+                new CustomerCreatedEvent(id, fullName);
+        var outbox = new Outbox();
+        outbox.setPayload(objectMapper.writeValueAsString(customerCreatedEvent));
+        outbox.setDestination("validation");
+        outboxRepository.save(outbox);
+        //kafkaPublisher.publishMessage(CUSTOMER_CREATION_TOPIC, customerCreatedEvent);
     }
 
     @Override
